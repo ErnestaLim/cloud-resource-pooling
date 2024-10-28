5b4159cc65b825098ae151fd4292a690b2608f81
tejaboy <2957895l@student.gla.ac.uk>
2024-10-28 17:17:31 +0800

    Start Dask Scheduler via Python

--- Master/socket_master.py
+++ Master/socket_master.py
@@ -1,11 +1,13 @@
 import json
 import socket
 import time
+import asyncio
+from dask.distributed import Scheduler, Worker, Client
 
 def master_client_program():
     # host = '192.168.1.100'  # Server IP
     # port = 5000  # Server port
-    host = socket.gethostname() # Initiate connection to server
+    host = socket.gethostbyname(socket.gethostname()) # Initiate connection to server
     port = 5000  # Server port number    
     client_socket = socket.socket() # Initiate connection to server
     client_socket.connect((host, port))    
@@ -14,25 +16,37 @@ def master_client_program():
     identification_data = "master"
     client_socket.send(identification_data.encode())
 
+    # Flag
+    broker_started: bool = False
+
     # Maintain connection till Server resolves client distribution
     while True:
-        # Simulate waiting for other script to call
-        time.sleep(1)
-        client_socket.send("request;1".encode())
-        print("Requesting central server for 1 slave ...")
+        if not broker_started:
+            # Simulate waiting for other script to call
+            time.sleep(1)
+            client_socket.send("request;1".encode())
+            print("Requesting central server for 1 slave ...")
 
-        # Wait for response (blocking call)
-        response = client_socket.recv(1024).decode()
-        response_data = json.loads(response)
+            # Wait for response (blocking call)
+            response = client_socket.recv(1024).decode()
+            response_data = json.loads(response)
 
-        if response_data['success'] == False :
-            print(f"Error: {response_data['message']}")
-            print("Request failed. Retrying ...")
+            if response_data['success'] == False :
+                print(f"Error: {response_data['message']}")
+                print("Request failed. Retrying ...")
+            else:
+                broker_started = True
+                print(f"Centeral server provided {len(response_data['addresses'])} nodes. Waiting for slave to connect ...")
+                asyncio.get_event_loop().run_until_complete(master_loop())
         else:
-            print(f"Centeral server provided {len(response_data['addresses'])} nodes. Waiting for slave to connect ...")
-            break
+            print("test")
 
     client_socket.close()  # close the connection
+
+async def master_loop():
+    s = Scheduler(host=socket.gethostbyname(socket.gethostname()), port=8786)        # scheduler created, but not yet running
+    s = await s            # the scheduler is running
+    await s.finished()     # wait until the scheduler closes
 
 if __name__ == '__main__':
     master_client_program()
