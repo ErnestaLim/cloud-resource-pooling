64fea64dc44548b801970df2510e07391e0e884e
tejaboy <2957895l@student.gla.ac.uk>
2024-11-04 01:09:59 +0800

    Allow dynamic username

--- send.py
+++ send.py
@@ -3,7 +3,7 @@ import pickle
 import time
 import socket
 
-def recursive_call(llm_name: str):
+def recursive_call(username: str, llm_name: str):
     reconnection_flag = 1
 
     while reconnection_flag == 1:
@@ -12,7 +12,7 @@ def recursive_call(llm_name: str):
             client_socket.connect(('192.168.1.5', 8786))
             reconnection_flag = 0
             print("Connected to server. Sending task ...")
-            client_socket.send(f"do_llm_eval;bernard;{llm_name}".encode()) # Send request
+            client_socket.send(f"do_llm_eval;{username};{llm_name}".encode()) # Send request
             print("Task sent. Awaiting results ...")
 
             while True: 
@@ -43,7 +43,8 @@ def recursive_call(llm_name: str):
 if __name__ == "__main__":
     # Set up argument parser
     parser = argparse.ArgumentParser(description='Client program to connect to a server.')
+    parser.add_argument('--username', type=str, default="guest", help='Username to use for the task')
     parser.add_argument('--llm_name', type=str, default="EleutherAI/pythia-160m", help='LLM namespace from Hugging Face')
     args = parser.parse_args()
     
-    recursive_call(args.llm_name)
+    recursive_call(args.username, args.llm_name)
